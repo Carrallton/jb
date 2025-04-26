827dc419c0779dd961fc8f1f949ce0482b252146
JamesBroover <dbroover@yandex.ru>
2025-04-27 01:23:24 +0300

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'ckeditor',
+    'ckeditor_5',
     'corsheaders',
     'storages',
     'blog.apps.BlogConfig',
@@ -64,23 +64,23 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'myblog',
-#        'USER': 'mybloguser',
-#        'PASSWORD': 'mypassword',
-#        'HOST': 'db',
-#        'PORT': '5432',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myblog',
+        'USER': 'broover',
+        'PASSWORD': 'Yumshanov39',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
